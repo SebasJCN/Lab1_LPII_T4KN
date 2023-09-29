20978eaec1e03f6b80a178af88f7800c9d681b78
SebasJCN <tebsss_52@hotmail.com>
2023-09-28 21:26:49 -0500

    Archivos agregados

--- com.CL1/src/main/java/views/EmpleadoView.java
+++ com.CL1/src/main/java/views/EmpleadoView.java
@@ -6,9 +6,9 @@ public class EmpleadoView {
 
 	public static void main(String[] args) {
 		//String empleado = new EmpleadoController().createEmpleado("Cruz Domínguez","Juan Antonio","32","Masculino",2150.50);
-		//String empleado = new EmpleadoController().deleteEmpleado(2);
-		//String empleado = new EmpleadoController().updateEmpleado(3, "Cruz Domingo", "José Anthon", "25", "Masculino", 3000.0);
-		String empleado = new EmpleadoController().getEmpleado(3);
+		String empleado = new EmpleadoController().deleteEmpleado(5);
+		//String empleado = new EmpleadoController().updateEmpleado(5, "Palacios Sáenz", "Maria Julia", "25", "Femenino", 3000.0);
+		//String empleado = new EmpleadoController().getEmpleado(5);
 		System.out.println(empleado);
 	}
 
